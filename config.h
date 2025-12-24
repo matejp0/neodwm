e3fdad648bdad4477c3f1dc6a7edf049d7aa8dbb
Matěj Přerovský <bofin@skaut.cz>
2025-12-24 13:07:39 +0100

    replaced long keyboard-changing command with a script

--- config.h
+++ config.h
@@ -124,7 +124,7 @@ static const Key keys[] = {
 	{ 0,														XF86XK_MonBrightnessDown,	 spawn,		SHCMD("/usr/bin/brightnessctl set 5%- && pkill -RTMIN+7 dwmblocks") },
 	{ 0,														XF86XK_MonBrightnessUp,		 spawn,		SHCMD("/usr/bin/brightnessctl set 5%+ && pkill -RTMIN+7 dwmblocks") },
 
-	{ ALTKEY|ShiftMask,							XK_Return, spawn,					 SHCMD("setxkbmap -query | grep -q 'us' && setxkbmap -layout cz -variant qwerty || setxkbmap us; pkill -RTMIN+11 dwmblocks ") },
+	{ ALTKEY|ShiftMask,							XK_Return, spawn,					 SHCMD("~/.local/share/dwm/dwmblocks-async/change-kblayout.sh") },
 	{ 0,														XK_Print,	 spawn,					 SHCMD("xfce4-screenshooter") },
 	{ MODKEY,												XK_a,			 spawn,					 SHCMD("/usr/bin/autorandr --change && dunstify -r 15 'autorandr' 'reload'") },
 	{ MODKEY|ShiftMask,							XK_r, 		 spawn,					 SHCMD("~/.local/share/dwm/neodwm/toggle-rotate.sh") },
