7d9ae55e778f660e6a1c071d74b96c86ebf9b101
Matěj Přerovský <bofin@skaut.cz>
2026-01-06 22:36:37 +0100

    changed keybinding to toggle floating because it had been the same as the one for restarting librewolf

--- config.h
+++ config.h
@@ -149,7 +149,7 @@ static const Key keys[] = {
 //	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[4]} },
 // 	{ MODKEY,                       XK_space,  setlayout,      {} },
-	{ MODKEY|ShiftMask,             XK_f,  		 togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_m,  		 focusmon,       {.i = +1 } },
